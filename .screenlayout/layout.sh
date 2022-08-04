8b48d6bf6986c1db77b305aa2aa0023b5d91c814
Streppel <streppels@gmail.com>
2022-08-04 12:12:39 -0300

    screen layout script

--- .screenlayout/layout.sh
+++ .screenlayout/layout.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 c=($(xrandr --query | grep ' connected' | awk '{print $1}'))
-xrandr --output eDP1 --primary --mode 1920x1200 --pos 0x0 --rotate normal
 if ((${#c[@]} > 1)); then
-	xrandr	--output DP1 --off \
+	xrandr --output eDP1 --primary --mode 1920x1200 --pos 0x1080 --rotate normal \
+		--output DP1 --off \
 		--output DP2 --off \
 		--output DP3 --mode 1920x1080 --pos 0x0 --rotate normal \
 		--output DP4 --off \
 		--output HDMI1 --off \
-		--output VIRTUAL1 --of
+		--output VIRTUAL1 --off
+else
+	xrandr --output eDP1 --primary --mode 1920x1200 --pos 0x0 --rotate normal \
+		--output DP1 --off \
+		--output DP2 --off \
+		--output DP3 --off \
+		--output DP4 --off \
+		--output HDMI1 --off \
+		--output VIRTUAL1 --off
 fi
 notify-send "screen resolution updated!"
 
